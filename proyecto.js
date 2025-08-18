255effdc4f9a3017722ebf95b82ddb5a3e5aaaed
gabrielhaches <ignaciohaches.89@gmail.com>
2025-08-18 10:04:43 -0400

    proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -19,8 +19,53 @@ conexion.connect((err) => {
     console.log('Conexión exitosa a la base de datos');
 });
 
-app.get('/mascotas', (req, res) => {
-    let sql = 'SELECT * FROM Animales';
+app.get('/adopciones', (req, res) => {
+    let sql = 'SELECT * FROM adopciones';
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.error("Error en la consulta:", err);
+            res.status(500).send('Error en la consulta a la base de datos');
+            return;
+        }
+        res.json(result);
+    });
+});
+app.get('/animales', (req, res) => {
+    let sql = 'SELECT * FROM animales';
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.error("Error en la consulta:", err);
+            res.status(500).send('Error en la consulta a la base de datos');
+            return;
+        }
+        res.json(result);
+    });
+});
+app.get('/donaciones', (req, res) => {
+    let sql = 'SELECT * FROM  donaciones';
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.error("Error en la consulta:", err);
+            res.status(500).send('Error en la consulta a la base de datos');
+            return;
+        }
+        res.json(result);
+    });
+});
+app.get('/historial_medico', (req, res) => {
+    let sql = 'SELECT * FROM historial_medico';
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.error("Error en la consulta:", err);
+            res.status(500).send('Error en la consulta a la base de datos');
+            return;
+        }
+        res.json(result);
+    });
+});
+
+app.get('/personas', (req, res) => {
+    let sql = 'SELECT * FROM personas';
     conexion.query(sql, (err, result) => {
         if (err) {
             console.error("Error en la consulta:", err);
